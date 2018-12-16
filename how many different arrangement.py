ac8cee03e30ea164c3be5dadebf0217227f7d327
tomcalach <45789099+tomcalach@users.noreply.github.com>
2018-12-16 12:21:49 +0200

    Add files via upload

---
+++ how many different arrangement.py
@@ -0,0 +1,36 @@
+def stupid_check(n):
+    """
+    checking how many different ways are there to arrange 1|2 in n size
+    :param n: int
+    :return: int
+    """
+
+    if n < 0:
+        return 0
+    if n == 0 or n == 0:
+        return 1
+    else:
+        return stupid_check(n-1) + stupid_check(n-2)
+
+
+def dp_check(n, mem={}):
+    """
+    checking how many different ways are there to arrange 1|2 in n size, using memo and DP
+    :param n: int
+    :param mem: dict
+    :return: int
+    """
+
+    if mem.get(n, 0):
+        return mem[n]
+    if n < 0:
+        return 0
+    if n == 0 or n == 0:
+        return 1
+    else:
+        count = dp_check(n-1) + dp_check(n-2)
+        mem[n] = count
+        return count
+
+
+
